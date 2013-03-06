a7f4ad9e381aef1b47d7c874a3d33f1c544f4725
Matthew Hooker <mwhooker@gmail.com>
2013-03-06 14:11:03 -0800

    todo

--- cookbook/metadata.rb
+++ cookbook/metadata.rb
@@ -5,6 +5,7 @@ description      "Installs/Configures jones"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 name             "jones"
 version          "0.1.0"
+# TODO: depend on build-essential if we're on a debian-like
 depends          "python"
 depends          "nginx_conf"
 depends          "zookeeper"
