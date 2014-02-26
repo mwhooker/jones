9c9ecdcaf839fb05dc6d1c5b0c0dd000ebb78d7d
Dexter Legaspi <dalegaspi@gmail.com>
2014-02-25 19:05:11 -0500

    Simple Jones Client for .NET
    A simple Jones Client for .NET 4.0+

---
+++ contrib/dotnet/JonesClientTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JonesClient;
+using Sodao.Zookeeper;
+
+namespace JonesClientTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCreateSimpleJonesClient()
+        {
+            var jc = new SimpleJonesClient(service: "yoda");
+
+            //while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+
+                foreach (var key in jc.Config.Keys)
+                {
+                    Console.WriteLine("{0} = {1}", key, jc.Config[key]);
+                }
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ contrib/dotnet/JonesClientTestConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using JonesClient;
+
+namespace JonesClientTestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jc = new SimpleJonesClient(service: "yoda", callback: (d => {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("config has changed: we now have {0} config items", d.Keys.Count);
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+                foreach (var k in d.Keys)
+                {
+                    Console.WriteLine("{0} = {1}", k, d[k]);
+                }
+
+                Console.WriteLine("-");
+
+            }));
+
+            Console.WriteLine("hello, world!");
+            Console.WriteLine("service {0} has {1} config items", jc.ServiceName, jc.Config.Keys.Count);
+            foreach (var key in jc.Config.Keys)
+            {
+                Console.WriteLine("{0} = {1}", key, jc.Config[key]);
+            }
+
+            Console.WriteLine("-");
+            Console.ReadKey(false);
+        }
+    }
+}
